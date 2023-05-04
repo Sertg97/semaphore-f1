17306c97e344d4ba451d7d08ea9e8b400854a7c1
Sergio P <skx197x@gmail.com>
2023-05-03 23:22:45 -0500

    v3 Secuencia de semáforos rojos
    Se agrego la secuencia de semáforos rojos y al pasar el tiempo se devuelve al default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,17 +28,14 @@ export class AppComponent {
       this.isRed = true;
   
       setTimeout(() => {
-        //light1.classList.remove("red");
         light1.classList.add("red");
         light2.classList.add("red");
   
         setTimeout(() => {
-          //light2.classList.remove("red");
           light3.classList.add("red");
           light4.classList.add("red");
   
           setTimeout(() => {
-            //light3.classList.remove("red");
             light5.classList.add("red");
             light6.classList.add("red");
   
@@ -50,39 +47,60 @@ export class AppComponent {
                 light9.classList.add("red");
                 light10.classList.add("red");
 
-                /*setTimeout(() => {
-                  //light4.classList.remove("red");
-                  light1.classList.add("color-default");
-                  light2.classList.add("color-default");
-                  light3.classList.add("color-default");
-                  light4.classList.add("color-default");
-                  light5.classList.add("color-default");
-                  light6.classList.add("color-default");
-                  light7.classList.add("color-default");
-                  light8.classList.add("color-default");
-                  light9.classList.add("color-default");
-                  light10.classList.add("color-default");
+                setTimeout(() => {
+                  this.changeToDefault();
                   this.isRed = false;
-                }, 1000);*/
-              }, 1000);
-            }, 1000);
-          }, 1000);
-        }, 1000);
-      }, 1000);
+                }, 4000);
+              }, 2000);
+            }, 2000);
+          }, 2000);
+        }, 2000);
+      }, 2000);
     }
   }
 
-  /*changeColor() {
+  changeToDefault() {
     const light1 = document.getElementById("light1");
     const light2 = document.getElementById("light2");
     const light3 = document.getElementById("light3");
     const light4 = document.getElementById("light4");
-    
-    if (light1 && light2 && light3 && light4) {
-      light1.classList.add("red");
-      light2.classList.add("red");
-      light3.classList.add("red");
-      light4.classList.add("red");
+    const light5 = document.getElementById("light5");
+    const light6 = document.getElementById("light6");
+    const light7 = document.getElementById("light7");
+    const light8 = document.getElementById("light8");
+    const light9 = document.getElementById("light9");
+    const light10 = document.getElementById("light10");
+  
+    if (light1 && light2 && light3 && light4 && light5 && light6 && light7 && light8 && light9 && light10) {
+    light1.classList.remove("red");
+    light2.classList.remove("red");
+    light3.classList.remove("red");
+    light4.classList.remove("red");
+    light5.classList.remove("red");
+    light6.classList.remove("red"); 
+    light7.classList.remove("red");
+    light8.classList.remove("red");
+    light9.classList.remove("red");
+    light10.classList.remove("red");
+
+    light1.classList.add("color-default");
+    light2.classList.add("color-default");
+    light3.classList.add("color-default");
+    light4.classList.add("color-default");
+    light5.classList.add("color-default");
+    light6.classList.add("color-default");
+    light7.classList.add("color-default");
+    light8.classList.add("color-default");
+    light9.classList.add("color-default");
+    light10.classList.add("color-default");
     }
-  }*/
+  }
+
 }
+
+
+
+
+
+
+
